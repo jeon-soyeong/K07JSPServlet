5f06abdbab5cb9e552a3d28419e1d84676b90fe0
jsu3417 <jsu3417@naver.com>
2020-05-18 18:27:49 +0900

    5/18

--- src/controller/DataroomDAO.java
+++ src/controller/DataroomDAO.java
@@ -106,5 +106,131 @@ public class DataroomDAO {
 		return bbs;
 	}
 	
+	//자료실 글쓰기 처리
+	public int insert(DataroomDTO dto) {
+		
+		int affected = 0;
+		
+		try {
+			String sql = "INSERT INTO dataroom ("
+					+ " idx,title,name,content,attachedfile,pass,downcount) "
+					+ " VALUES ("
+					+ " dataroom_seq.nextval,?,?,?,?,?,0)";
+			System.out.println(sql);
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getName());
+			psmt.setString(3, dto.getContent());
+			psmt.setString(4, dto.getAttachedfile());
+			psmt.setString(5, dto.getPass());
+			
+			//insert성공시 '1', 실패시 '0' 반환
+			affected = psmt.executeUpdate();		
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return affected;
+	}
+	
+	//일련번호에 해당하는 게시물을 가져와서 DTO객체에 저장후 반환
+		//자료실 게시물 상세보기
+		public DataroomDTO selectView(String idx) {
+			
+			DataroomDTO dto = null;
+			
+			String sql = "SELECT * FROM dataroom WHERE idx=?";
+			
+			try {
+				psmt = con.prepareStatement(sql);
+				psmt.setString(1, idx);
+				rs = psmt.executeQuery();
+				if(rs.next()) {
+					dto = new DataroomDTO();
+					
+					dto.setIdx(rs.getString(1));
+					dto.setName(rs.getString(2));
+					dto.setTitle(rs.getString(3));
+					dto.setContent(rs.getString(4));
+					dto.setPostdate(rs.getDate(5));
+					dto.setAttachedfile(rs.getString(6));
+					dto.setDowncount(rs.getInt(7));
+					dto.setPass(rs.getString(8));
+					dto.setVisitcount(rs.getInt(9));//조회수추가
+				
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			return dto;
+		}
+	
+	//조회수 증가
+	public void updateVisitCount(String idx) {
+		String sql = "UPDATE dataroom SET "
+				+" visitcount=visitcount+1 "
+				+" WHERE idx=?";
+		try {
+			psmt=con.prepareStatement(sql);
+			psmt.setString(1, idx);
+			psmt.executeQuery();
+		} catch (Exception e) {}
+	}
+	
+	public boolean isCorrectPassword(String pass, String idx) {
+		
+		boolean isCorr = true;
+		try {
+			String sql = "SELECT COUNT(*) FROM dataroom WHERE pass=? AND idx=?";
+				psmt = con.prepareStatement(sql);
+				psmt.setString(1, pass);
+				psmt.setString(2, idx);
+				rs = psmt.executeQuery();
+				rs.next();
+				if(rs.getInt(1)==0) {
+					isCorr = false;
+				}
+		} catch (Exception e) {
+			isCorr = false;
+			e.printStackTrace();
+		}
+		return isCorr;
+	}
+	
+	
+	public int delete(String idx) {
+		
+		int affected = 0;
+		try {
+			String query = "DELETE FROM dataroom WHERE idx=?";
+					
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, idx);
+			
+			affected = psmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("delete중 예외발생");
+			e.printStackTrace();
+		}
+		return affected;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
